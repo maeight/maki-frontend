a9aaf7669c4394c8715ced4f5bc3eed7824cd43c
Uni <cryptounico@gmail.com>
2021-05-29 00:11:45 -0500

    add: mainnet

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -2,17 +2,47 @@ import contracts from './contracts'
 import { FarmConfig, QuoteToken } from './types'
 
 const farms: FarmConfig[] = [
+  {
+    pid: 1,
+    lpSymbol: 'MAKI-HT',
+    lpAddresses: {
+      256: '0xa0af5d360232e077decfd4650e8b95875fdd6aad', // UPDATE
+      128: '0xC923E7Dd24A96Da2136Cbc3C99F544F225A46424', // UPDATED
+    },
+    tokenSymbol: 'MAKI',
+    tokenAddresses: {
+      256: '0x6858a26bBBc8e185274969f6baf99674929Cf766', // UPDATED
+      128: '0x5fad6fbba4bba686ba9b8052cf0bd51699f38b93', // UPDATED - ABI UPDATED
+    },
+    quoteTokenSymbol: QuoteToken.HT,
+    quoteTokenAdresses: contracts.wht,
+  },
+  {
+    pid: 2,
+    lpSymbol: 'LAYER-HT',
+    lpAddresses: {
+      256: '0xa0af5d360232e077decfd4650e8b95875fdd6aad', // Is correct?
+      128: '0xc189d2699c7e077cb050d9bc666effa40bb31771', // UPDATED
+    },
+    tokenSymbol: 'LAYER',
+    tokenAddresses: {
+      256: '0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c', // UPDATE
+      128: '0x639a647fbe20b6c8ac19e48e2de44ea792c62c5c', // UPDATED - ABI UPDATED
+    },
+    quoteTokenSymbol: QuoteToken.HT,
+    quoteTokenAdresses: contracts.wht,
+  },
   // {
-  //   pid: 1,
-  //   lpSymbol: 'MAKI-HT',
+  //   pid: 3,
+  //   lpSymbol: 'MAKI-HUSD',
   //   lpAddresses: {
-  //     256: '0xa0af5d360232e077decfd4650e8b95875fdd6aad', // Is correct?
+  //     256: '', // Is correct?
   //     128: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6', // UPDATE
   //   },
   //   tokenSymbol: 'MAKI',
   //   tokenAddresses: {
-  //     256: '0x6858a26bBBc8e185274969f6baf99674929Cf766', // Is correct?
-  //     128: '0x5fad6fbba4bba686ba9b8052cf0bd51699f38b93', // UPDATED - ABI UPDATED
+  //     256: '0x6858a26bBBc8e185274969f6baf99674929Cf766', UPDATED
+  //     128: '', // UPDATED - ABI UPDATED
   //   },
   //   quoteTokenSymbol: QuoteToken.HT,
   //   quoteTokenAdresses: contracts.wht,
