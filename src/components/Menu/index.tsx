27ece25b502b4c8be19f339ad6b3e650b82b17f3
Uni <cryptounico@gmail.com>
2021-06-13 18:45:37 -0500

    add: HECO prompt

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Menu as UikitMenu } from 'maki-uikit'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
 import { allLanguages } from 'config/localisation/languageCodes'
 import { LanguageContext } from 'contexts/Localisation/languageContext'
 import useTheme from 'hooks/useTheme'
 import { usePriceMakiHusd, useProfile } from 'state/hooks'
+import { HECO_MAINNET } from 'config/constants/metamask_network'
 import config from './config'
 
 const Menu = (props) => {
-  const { account, connect, reset } = useWallet()
+  const { account, ethereum, connect, reset } = useWallet()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
   const makiPriceUsd = usePriceMakiHusd()
   const { profile } = useProfile()
 
+  useEffect(() => {
+    (window as any).ethereum
+        .request(HECO_MAINNET)
+        // .then(() => {console.log('ok done')})
+        // .catch((error: any) => {
+        //   console.log(error)
+        // })
+
+  }, [])
+ 
   return (
     <UikitMenu
       account={account}
+      provider={ethereum}
       login={connect}
       logout={reset}
       isDark={isDark}
