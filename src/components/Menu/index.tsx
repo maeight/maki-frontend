38c87afd7830cb40ac77b534f353d545b7fff0ca
Uni <CryptoUnico@gmail.com>
2021-06-14 20:32:54 -0500

    Revert "update: HECO connect"
    This reverts commit 18fb24408d5f13bb2e4ca3e4b8961f52715a6532.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react'
 import { Menu as UikitMenu } from 'maki-uikit'
-import { LanguageContext } from 'contexts/Localisation/languageContext'
-import { HECO_MAINNET } from 'config/constants/metamask_network'
-import useTheme from 'hooks/useTheme'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { provider } from 'web3-core'
-// import useAuth from 'hooks/useAuth'
+import { allLanguages } from 'config/localisation/languageCodes'
+import { LanguageContext } from 'contexts/Localisation/languageContext'
+import useTheme from 'hooks/useTheme'
 import { usePriceMakiHusd, useProfile } from 'state/hooks'
+import { HECO_MAINNET } from 'config/constants/metamask_network'
 import config from './config'
 
-
 const Menu = (props) => {
-  const { account, connect, reset } = useWallet()
+  const { account, ethereum, connect, reset } = useWallet()
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
   const makiPriceUsd = usePriceMakiHusd()
@@ -20,11 +18,13 @@ const Menu = (props) => {
   return (
     <UikitMenu
       account={account}
+      provider={ethereum}
       login={connect}
       logout={reset}
       isDark={isDark}
       toggleTheme={toggleTheme}
       currentLang={selectedLanguage && selectedLanguage.code}
+      langs={allLanguages}
       setLang={setSelectedLanguage}
       makiPriceUsd={makiPriceUsd.toNumber()}
       links={config}
